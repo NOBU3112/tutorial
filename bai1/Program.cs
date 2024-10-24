33f3071986825ef2fba4038adc2b7dc30b16ba77
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-24 20:51:34 +0700

    bai1

---
+++ bai1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;//Xuất được tiếng việt
+            Console.Write("Hello");
+            Console.WriteLine("Xin chào, mình mới học C#");
+            Console.WriteLine(200);
+            //Console.ReadLine();
+            Console.ReadKey(true);
+            Console.WriteLine("Họ và tên : Nguyễn Tuấn Đạt");
+            Console.WriteLine("Năm sinh: " + 31 + "/" + 12 + "/" + 2003);
+            Console.WriteLine("Giới tính: Nam");
+            Console.WriteLine("Tuổi của bạn : 20");
+            Console.ReadLine();
+        }
+    }
+}
