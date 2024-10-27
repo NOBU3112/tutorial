d9a604de82a539109c4da65c7bfac195c918b733
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-27 13:12:13 +0700

    bai6

---
+++ ToanTuLogicVaPrefixPostfix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToanTuLogicVaPrefixPostfix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kq;
+            int a = 6, b = 10;
+            kq = !((a!=b) && (a<3));
+            Console.WriteLine("Gia tri cua ket qua la: " + kq);
+            Console.ReadKey();
+
+            int x = 1, y = 2;
+            int z = x++ - ++y + 1;
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("z = " +  z);
+            Console.ReadKey();
+        }
+    }
+}
