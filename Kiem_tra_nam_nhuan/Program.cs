a20f57a63c939070ef505680c64d82282d49ffea
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-28 14:04:57 +0700

    bai 6

---
+++ Kiem_tra_nam_nhuan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiem_tra_nam_nhuan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int nam;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Mời nhập vào 1 năm bất kỳ: ");
+            nam = int.Parse(Console.ReadLine());
+            if((nam % 4 == 0 && nam % 100 != 0) || nam % 400 == 0)
+                Console.WriteLine("{0} là năm nhuận", nam);
+            else
+                Console.WriteLine("{0} không phải là năm nhuận", nam);
+            Console.ReadKey(); 
+        }
+    }
+}
