8b2ad42ce663bfcc5a5ec44e10dc7078fcbdf1c9
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-28 23:18:02 +0700

    bai 8

---
+++ Toan_tu_ba_ngoi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toan_tu_ba_ngoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //nhập vào một số n: kiểm tra n là chẵn hay lẻ
+            Console.OutputEncoding = Encoding.UTF8;
+            int n;
+            Console.WriteLine("Nhap vao so nguyen n:");
+            n = int.Parse(Console.ReadLine());
+            String traLoi = (n % 2 == 0) ? "Số chẵn" : "Số lẻ"; // biến = (đk)? nếu đúng : nếu sai
+            Console.WriteLine("Số {0} là {1}",n,traLoi);
+            Console.ReadLine();
+        }
+    }
+}
