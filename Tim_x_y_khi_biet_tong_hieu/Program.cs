4470e1d393f222811e73e7b90fe68c4d92a90f8d
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-28 13:09:32 +0700

    bt 4

---
+++ Tim_x_y_khi_biet_tong_hieu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tim_x_y_khi_biet_tong_hieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double tong, hieu;
+            Console.WriteLine("Nhập vào tổng hai số");
+            tong = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập vào hiệu hai số");
+            hieu = double.Parse(Console.ReadLine());
+            double x = (tong + hieu) / 2;
+            double y = tong - x;
+            Console.WriteLine("Gía trị của x cần tìm là:" + x);
+            Console.WriteLine("Gía trị của y cần tìm là:" + y);
+            Console.ReadLine();
+        }
+    }
+}
