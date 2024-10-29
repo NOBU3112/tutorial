67831d143476971bdaeed39d98938a463130695b
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-29 15:26:39 +0700

    bt 16

---
+++ S=1!+2!++10!/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_1__2___10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 1;
+            int tong = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                m = i * m;
+                Console.WriteLine(m);
+                //1 m = 1 = 1!
+                //2 m = 2 = 2!
+                //3 m = 6 = 6!
+                tong = tong + m;
+            }
+            Console.WriteLine("Tong s= " + tong);
+            Console.ReadKey();
+        }
+    }
+}
