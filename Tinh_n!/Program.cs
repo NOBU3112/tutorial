2397fafde852527a7e84bac65a48d4dd4b811aeb
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-29 14:42:49 +0700

    bt 12

---
+++ Tinh_n!/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tinh_n_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.OutputEncoding = Encoding.UTF8;
+            int n, kq = 1;
+            Console.WriteLine("Nhập vào n:");
+            n = int.Parse(Console.ReadLine());
+            //Dùng for
+            for (int i=1; i<=n ; i++) 
+            {
+                Console.WriteLine("i = " + i);
+                kq *= i;
+            }
+            Console.WriteLine("Kết quả giai thừa của {0} là: {1}", n, kq);
+
+            //Dùng while
+            int j = 1;
+            kq = 1;
+            while (j <= n)
+            {
+                Console.WriteLine("j = " + j);
+                kq *= j;
+                j++;
+            }
+            Console.WriteLine("Kết quả giai thừa của {0} là: {1}", n, kq);
+            Console.ReadKey();
+        }
+    }
+}
