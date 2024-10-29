eb49b5ee79190a7b389565b488c2bfcff968f7e4
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-29 15:11:28 +0700

    bt 15

---
+++ Tim_so_chia_het_cho_3_tu_10_den_50/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tim_so_chia_het_cho_3_tu_10_den_50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 10; i<=50;i++)
+            {
+                if(i%3 == 0)
+                {
+                    Console.Write(i + " "); //viết trên 1 dòng
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
