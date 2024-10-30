f5785570174fa35cbee518f7418fd8238e2ea19e
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-30 21:15:10 +0700

    bt 21

---
+++ Chuyen_tin_nhan_sang_mat_ma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuyen_tin_nhan_sang_mat_ma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string a = "abcdefghijklmnopqrstuvwxyz";
+            string b = "zxcvbnmasdfghjklqwertyuiop";
+            Console.WriteLine("Mời nhập vào chuỗi cần mã hóa");
+            string chuoiInput = Console.ReadLine();
+            string chuoiOutput = "";
+            foreach (char c in chuoiInput) 
+            {
+                int indexKytu = a.IndexOf(c);
+                chuoiOutput += b[indexKytu];
+            }
+            Console.WriteLine("Chuỗi trước mã hóa là: " + chuoiInput);
+            Console.WriteLine("Chuỗi sau mã hóa là: " + chuoiOutput);
+            Console.ReadLine();
+        }
+    }
+}
