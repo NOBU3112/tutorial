6ae5879936d6615a6e73bcbc4ab1227f83f39a0a
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-30 21:48:46 +0700

    bt 23

---
+++ Tach_so_va_chu_thanh_2_chuoi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tach_so_va_chu_thanh_2_chuoi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string chuoi, chuoiSo ="", chuoiChu ="";
+            Console.WriteLine("Nhập vào một chuỗi: ");
+            chuoi = Console.ReadLine();
+            foreach(char c in chuoi) 
+            {
+                if (char.IsDigit(c))
+                    chuoiSo += c;
+                else if (char.IsLetter(c))
+                    chuoiChu += c;
+                else if (char.IsWhiteSpace(c))
+                {
+                    chuoiSo += c;
+                    chuoiChu += c;
+                }    
+
+            }
+            Console.WriteLine("Chuỗi chỉ gồm số là: " + chuoiSo);
+            Console.WriteLine("Chuỗi chỉ gồm chư cái là: " +chuoiChu);
+            Console.ReadKey();
+
+        }
+    }
+}
