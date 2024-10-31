da83cec11046f33274b1589c96801d6fd69d2603
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-10-31 21:43:23 +0700

    bt 24

---
+++ Tao_list_random/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tao_list_random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int n;
+            Random rnd = new Random();
+            Console.WriteLine("Nhập số phần tử n: ");
+            n = int.Parse(Console.ReadLine());
+            List<int> lst = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                lst.Add(rnd.Next(101));
+            }
+            foreach (int i in lst)
+            {
+                Console.Write(i + "\t");
+            }
+            Console.ReadKey();
+        }
+    }
+}
