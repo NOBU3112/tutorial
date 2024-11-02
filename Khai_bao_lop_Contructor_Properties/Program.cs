5c3ce7bbe4be8bc6f568deedd3232cf5e1fc42b7
Nguyễn Tuấn Đạt <158294658+NOBU3112@users.noreply.github.com>
2024-11-02 10:52:53 +0700

    bai 22-1

---
+++ Khai_bao_lop_Contructor_Properties/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khai_bao_lop_Contructor_Properties
+{
+    internal class Program
+    {
+        //Quy tắc đặt tên lớp
+        //1.Tên lớp nên là 1 danh từ hoặc một cụm DT,nên viết hoa chữ cái đầu
+        //2.Không bắt đầu bằng số, chữ cái  đặc biệt, không trùng với keyword
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            // Tạo 1 đối tượng mới không truyền giá trị
+            SinhVien sv1 = new SinhVien();
+            SinhVien sv2 = new SinhVien();
+
+            //Xuất
+            Console.WriteLine(sv1.MaSV);
+            Console.WriteLine(sv1.TenSV);
+            Console.WriteLine(sv2.MaSV);
+            Console.WriteLine(sv2.TenSV);
+            Console.WriteLine("Tên sinh viên là: " + sv1.TenSV);
+
+            // Taoij 1 đối tượng có truyền gias trị ban đầu.
+            SinhVien sv3 = new SinhVien(3,"Nguyễn Văn A");
+            Console.WriteLine("Tên sinh viên 3 là: " + sv3.TenSV);
+
+            //Sửa dữ liệu
+            sv3.MaSV = 123;
+            sv3.TenSV = "OFW";
+            Console.WriteLine("Tên sinh viên 3 là: " + sv3.TenSV);
+            Console.WriteLine("Mã sinh viên 3 là: " + sv3.MaSV);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Khai_bao_lop_Contructor_Properties/SinhVien.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khai_bao_lop_Contructor_Properties
+{
+    internal class SinhVien
+    {
+        //Khai báo biến lớp
+        private int maSV;
+        private string tenSV;
+
+        //Khai báo contructor
+        //(Gán giá trị mặc định cho đối tượng)
+        public SinhVien()
+        {
+            //Khi người dùng chưa gán giá trị vào thì đây là default
+            this.maSV = 0;
+            this.tenSV = "No name";
+        }
+
+        //Khai báo contructor
+        //(Khởi tạo giá trị cho đối tượng do người dùng truyền vào) cho phép gán giá trị 
+        public SinhVien(int maSV, string tenSV)
+        {
+            this.maSV = maSV;
+            this.tenSV = tenSV;
+        }
+
+        //Properties giúp ta có thể truy xuất xem,sửa đổi dữ liệu khi sử dụng private
+        public string TenSV
+        {
+            get { return tenSV; } //get giá trị để đọc
+            set { tenSV = value; }// set giá trị s
+        }
+        public int MaSV
+        {
+            get { return maSV; }
+            set { maSV = value; }
+        }
+    }
+}
